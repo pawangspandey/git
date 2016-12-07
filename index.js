4712c00a24e247ac3bdc638c96c05af4e765c4f1
pawangspandey <pawang.s.pandey@gmail.com>
2016-12-07 18:02:04 +0530

    fixed erro

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 console.log("Hello world.");
 var feature1 = require('./feature1.js');
 console.log("my first feature1",feature1);
-var feature1 = require('./feature2.js');
+var feature2 = require('./feature2.js');
 console.log("my first feature1",feature2);
